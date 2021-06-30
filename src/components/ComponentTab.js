e09256efed24e55218c8406e9546e1fe9203e905
VimuthM <vimuthmendis@gmail.com>
2021-07-01 00:10:46 +0800

    Fix hints for main inputs

--- src/components/ComponentTab.js
+++ src/components/ComponentTab.js
@@ -22,15 +22,20 @@ export default class ComponentTab extends Component {
                 <div id="toolbar_container_1_2_1" className="toolbarbuttonsContainer componentTab StringOps -1" style={{display:"none"}} />  
                 <div id="toolbar_container_1_2_1" className="toolbarbuttonsContainer componentTab Input 0">
                     
-                    <div id="addSlider" onClick={() => CreateNewSlider(this.props.context)} className="mainButtonItem 1 1" style={{backgroundImage: "url(https://image.flaticon.com/icons/png/512/983/983840.png)"}}>&nbsp;<span id="hint">Slider</span></div>
-                    <div id="addPanel" onClick={() => CreateNewPanel(this.props.context)} className="mainButtonItem 1 1" style={{backgroundImage: "url(https://storage.googleapis.com/ghostbucket111/icons/main_icons/2274978.png)"}}>&nbsp;<span id="hint">Panel</span></div>
-                    <div id="addToggle" onClick={() => CreateNewToggle(this.props.context)} className="mainButtonItem 1 1" style={{backgroundImage: "url(https://image.flaticon.com/icons/png/512/1465/1465907.png)"}}>&nbsp;<span id="hint">Toggle</span></div>
-                    <div id="addOptionList" onClick={() => CreateNewOptionList(this.props.context)} className="mainButtonItem 1 1" style={{backgroundImage: "url(https://image.flaticon.com/icons/png/512/1085/1085805.png)"}}>&nbsp;<span id="hint">Option list</span></div>
-                    <div id="addListView" onClick={() => CreateNewListView(this.props.context)} className="mainButtonItem 1 1" style={{backgroundImage: "url(https://storage.googleapis.com/ghostbucket111/icons/main_icons/checklist.png)"}}>&nbsp;<span id="hint">List view</span></div>
-                    <div id="addFile" onClick={() => CreateNewFileUpload(this.props.context)} className="mainButtonItem 1 1" style={{backgroundImage: "url(https://image.flaticon.com/icons/png/512/2329/2329379.png)"}}>&nbsp;<span id="hint">File upload</span></div>
+                    <div id="addSlider" onClick={() => CreateNewSlider(this.props.context)} className="mainButtonItem 1 1 tooltip" style={{backgroundImage: "url(https://image.flaticon.com/icons/png/512/983/983840.png)"}}>
+                        <span class="tooltiptext" id="hintx">Slider</span></div>
+                    <div id="addPanel" onClick={() => CreateNewPanel(this.props.context)} className="mainButtonItem 1 1 tooltip" style={{backgroundImage: "url(https://storage.googleapis.com/ghostbucket111/icons/main_icons/2274978.png)"}}>
+                        <span class="tooltiptext" id="hintx">Panel</span></div>
+                    <div id="addToggle" onClick={() => CreateNewToggle(this.props.context)} className="mainButtonItem 1 1 tooltip" style={{backgroundImage: "url(https://image.flaticon.com/icons/png/512/1465/1465907.png)"}}>
+                        <span class="tooltiptext" id="hintx">Toggle</span></div>
+                    <div id="addOptionList" onClick={() => CreateNewOptionList(this.props.context)} className="mainButtonItem 1 1 tooltip" style={{backgroundImage: "url(https://image.flaticon.com/icons/png/512/1085/1085805.png)"}}>
+                        <span class="tooltiptext" id="hintx">Option list</span></div>
+                    <div id="addListView" onClick={() => CreateNewListView(this.props.context)} className="mainButtonItem 1 1 tooltip" style={{backgroundImage: "url(https://storage.googleapis.com/ghostbucket111/icons/main_icons/checklist.png)"}}>
+                        <span class="tooltiptext" id="hintx">List view</span></div>
+                    <div id="addFile" onClick={() => CreateNewFileUpload(this.props.context)} className="mainButtonItem 1 1 tooltip" style={{backgroundImage: "url(https://image.flaticon.com/icons/png/512/2329/2329379.png)"}}>
+                        <span class="tooltiptext" id="hintx">File upload</span></div>
                     
-                </div>
-                
+                </div>           
                 <div id="toolbar_container_1_2_2" className="toolbarRightToggleNavigator 1" />
             </div>
         )
